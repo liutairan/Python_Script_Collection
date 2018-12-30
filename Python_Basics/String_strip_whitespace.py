832b7bc60e231034af98ab4b26d0899113a7a0a4
Tairan Liu <liutairan2012@gmail.com>
2018-12-29 22:12:52 -0600

    Create String_strip_whitespace.py

---
+++ Python_Basics/String_strip_whitespace.py
@@ -0,0 +1,10 @@
+# rstrip, lstrip, strip: will not change the string itself.
+str1 = " Test Test Test "
+
+
+# rstrip
+print(str1.rstrip())
+# lstrip
+print(str1.lstrip())
+# strip
+print(str1.strip())
