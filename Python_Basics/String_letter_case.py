522e578ca3a95bc714a1798c4fc976d61db9e695
Tairan Liu <liutairan2012@gmail.com>
2018-12-29 21:18:03 -0600

    Create String_letter_case.py

---
+++ Python_Basics/String_letter_case.py
@@ -0,0 +1,21 @@
+# Letter case related string operations
+
+# upper
+str1 = 'Test Test'
+print(str1.upper())
+# Output: 'TEST TEST'
+
+# lower
+str2 = 'Test Test'
+print(str2.lower())
+# Output: 'test test'
+
+# title
+str3 = 'Test test'
+print(str3.title())
+# Output: 'Test Test'
+
+# capitalize
+str4 = 'Test Test'
+print(str4.capitalize())
+# Output: 'Test test'
