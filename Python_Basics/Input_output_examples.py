7db0708c1f2a32df350a2ed0cc9fb1a88230e42b
Tairan Liu <liutairan2012@gmail.com>
2019-01-12 23:58:24 -0600

    Update Input_output_examples.py

--- Python_Basics/Input_output_examples.py
+++ Python_Basics/Input_output_examples.py
@@ -8,8 +8,10 @@ print(result2)
 # 10, A string
 # 10, A string
 
-#
-var3 = 1_234
+# Passing an integer after the ':' will cause that field
+# to be a minimum number of characters wide.
+# This is useful for making columns line up.
+var3 = 1_234 # This is equal to 1234
 var4 = 2_468
 var5 = var3/var4
 result3 = 'var3 is {:-5}, var5 is {:2.2%}'.format(var3,var5)
@@ -24,3 +26,19 @@ s1 = 'Hello world'
 print(str(s1))
 print(repr(s1))
 print(repr((10,20,('String 1','String 2'))))
+
+# Modifiers can be used to convert the value before it is formatted.
+# '!a' applies ascii(), '!s' applies str(), and '!r' applies repr()
+var6 = 'Test String'
+print(f'{var6}')
+print(f'{var6!r}')
+
+# String format() method
+print('{0}, {1}'.format('para1','para2'))
+print('{1}, {0}'.format('para1','para2'))
+print('{field1}, {field2}'.format(field1='para1',field2='para2'))
+print('{0}, {1}, {field3}'.format('para1','para2', field3='para3'))
+dict_var1 = {'field1': 111, 'field2': 222, 'field3': 333}
+print('field1: {0[field1]:d}; field2: {0[field2]:d}; '
+      'field3: {0[field3]:d}'.format(dict_var1))
+print('field1: {field1:d}; field2: {field2:d}; field3: {field3:d}'.format(**dict_var1))
