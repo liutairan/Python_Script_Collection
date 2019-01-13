b89df552617b2f2a23534273a4eb93d82d82ee7a
Tairan Liu <liutairan2012@gmail.com>
2019-01-12 23:41:25 -0600

    Update Input_output_examples.py

--- Python_Basics/Input_output_examples.py
+++ Python_Basics/Input_output_examples.py
@@ -15,3 +15,12 @@ var5 = var3/var4
 result3 = 'var3 is {:-5}, var5 is {:2.2%}'.format(var3,var5)
 print(result3)
 # var3 is  1234, var5 is 50.00%
+
+# The str() function is meant to return representations
+# of values which are fairly human-readable,
+# while repr() is meant to generate representations
+# which can be read by the interpreter
+s1 = 'Hello world'
+print(str(s1))
+print(repr(s1))
+print(repr((10,20,('String 1','String 2'))))
